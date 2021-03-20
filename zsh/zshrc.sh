f146bde32de91e302c98ab647b91baa5f26535bd
damian1997 <d.veltkamp137@gmail.com>
2021-03-20 20:50:33 +0100

    Added alacritty config and updated tmux and zsh

--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -1,149 +1,188 @@
-  # Set Spaceship ZSH as a prompt
+source ~/.zplug/init.zsh
 
-  # VARIABLES
-  export NOTES_DIR="$HOME/documents/notes"
+zplug "zsh-users/zsh-syntax-highlighting", defer:2
+zplug "zsh-users/zsh-autosuggestions", defer:2
+zplug "jeffreytse/zsh-vi-mode"
 
-  # ~/.zshrc
-  autoload -Uz compinit
-  if [ "$(whoami)" = "YOUR_NON_ADMIN_USER" ]; then
-    compinit -i # Ignore insecure directories
-  else
-    compinit
+if ! zplug check --verbose; then
+    printf "Install? [y/N]: "
+    if read -q; then
+        echo; zplug install
+    fi
+fi
+
+zplug load
+
+# VARIABLES
+export NOTES_DIR="$HOME/documents/notes"
+export NVM_DIR="$HOME/.nvm"
+  [ -s "$(brew --prefix)/opt/nvm/nvm.sh" ] && . "$(brew --prefix)/opt/nvm/nvm.sh" # This loads nvm
+  [ -s "$(brew --prefix)/opt/nvm/etc/bash_completion.d/nvm" ] && . "$(brew --prefix)/opt/nvm/etc/bash_completion.d/nvm" # This loads nvm bash_completion
+
+# ~/.zshrc
+autoload -Uz compinit
+if [ "$(whoami)" = "YOUR_NON_ADMIN_USER" ]; then
+  compinit -i
+else
+  compinit
+fi
+
+autoload -U promptinit; promptinit
+prompt spaceship
+
+# vi mode
+bindkey -v
+export KEYTIMEOUT=1
+
+# Change cursor shape for different vi modes.
+function zle-keymap-select {
+  if [[ ${KEYMAP} == vicmd ]] ||
+     [[ $1 = 'block' ]]; then
+    echo -ne '\e[2 q'
+  elif [[ ${KEYMAP} == main ]] ||
+       [[ ${KEYMAP} == viins ]] ||
+       [[ ${KEYMAP} = '' ]] ||
+       [[ $1 = 'beam' ]]; then
+    echo -ne '\e[6 q'
   fi
+}
+zle -N zle-keymap-select
+zle-line-init() {
+    zle -K viins # initiate `vi insert` as keymap (can be removed if `bindkey -V` has been set elsewhere)
+    echo -ne "\e[6 q"
+}
+zle -N zle-line-init
+echo -ne '\e[6 q' # Use beam shape cursor on startup.
+preexec() { echo -ne '\e[6 q' ;} # Use beam shape cursor for each new prompt.
 
-  autoload -U promptinit; promptinit
-  prompt spaceship
+# Custom cd
+chpwd() ls
 
-  export NVM_DIR="$HOME/.nvm"
-    [ -s "$(brew --prefix)/opt/nvm/nvm.sh" ] && . "$(brew --prefix)/opt/nvm/nvm.sh" # This loads nvm
-    [ -s "$(brew --prefix)/opt/nvm/etc/bash_completion.d/nvm" ] && . "$(brew --prefix)/opt/nvm/etc/bash_completion.d/nvm" # This loads nvm bash_completion
+# CD into development directory
+function development_directory() {
+  BUFFER="cd ~/documents/development"
+  zle accept-line
+}
 
+# CD into notes directory
+function notes_directory() {
+  BUFFER="cd ~/documents/notes"
+  zle accept-line
+}
 
-  # Custom cd
-  chpwd() ls
+# CD into nvim config directory
+function nvim_config_directory() {
+  BUFFER="cd ~/.config/nvim"
+  zle accept-line
+}
 
-  # CD into development directory
-  function development_directory() {
-    BUFFER="cd ~/documents/development"
-    zle accept-line
-  }
+# CD into home directory
+function home_directory() {
+  BUFFER="cd ~/."
+  zle accept-line
+}
+
+# CD up a directory
+function up_widget() {
+  BUFFER="cd .."
+  zle accept-line
+}
+
+function my_init() {
   zle -N development_directory
   bindkey "^p" development_directory
 
-	# CD up a directory
-	function up_widget() {
-		BUFFER="cd .."
-		zle accept-line
-	}
-	zle -N up_widget
-	bindkey "^k" up_widget
+  zle -N notes_directory
+  bindkey "^n" notes_directory
 
-  # git
-	function git_prepare() {
-		if [ -n "$BUFFER" ];
-			then
-				BUFFER="git add -A && git commit -m \"$BUFFER\" && git push"
-		fi
+  zle -N nvim_config_directory
+  bindkey "^v" nvim_config_directory
 
-		if [ -z "$BUFFER" ];
-			then
-				BUFFER="git add -A && git commit -v && git push"
-		fi
+  zle -N home_directory
+  bindkey "^h" home_directory
 
-		zle accept-line
-	}
-	zle -N git_prepare
-	bindkey "^g" git_prepare
+  zle -N up_widget
+  bindkey "^k" up_widget
+}
+zvm_after_init_commands+=(my_init)
 
-  ### PROCESS
-  # mnemonic: [K]ill [P]rocess
-  # show output of "ps -ef", use [tab] to select one or multiple entries
-  # press [enter] to kill selected processes and go back to the process list.
-  # or press [escape] to go back to the process list. Press [escape] twice to exit completely.
+### PROCESS
+# mnemonic: [K]ill [P]rocess
+# show output of "ps -ef", use [tab] to select one or multiple entries
+# press [enter] to kill selected processes and go back to the process list.
+# or press [escape] to go back to the process list. Press [escape] twice to exit completely.
+kp() {
+  local pid=$(ps -ef | sed 1d | eval "fzf ${FZF_DEFAULT_OPTS} -m --header='[kill:process]'" | awk '{print $2}')
+  if [ "x$pid" != "x" ]
+  then
+    echo $pid | xargs kill -${1:-9}
+  fi
+}
 
+bip() {
+  ### BREW + FZF
+  # update multiple packages at once
+  # mnemonic [B]rew [U]pdate [P]lugin
 
-  kp() {
-    local pid=$(ps -ef | sed 1d | eval "fzf ${FZF_DEFAULT_OPTS} -m --header='[kill:process]'" | awk '{print $2}')
-    if [ "x$pid" != "x" ]
-    then
-      echo $pid | xargs kill -${1:-9}
-    fi
-  }
+  local inst=$(brew search | eval "fzf ${FZF_DEFAULT_OPTS} -m --header='[brew:install]'")
 
-  bip() {
-    ### BREW + FZF
-    # update multiple packages at once
-    # mnemonic [B]rew [U]pdate [P]lugin
+  if [[ $inst ]]; then
+    for prog in $(echo $inst)
+    do brew install $prog
+    done
+  fi
+}
 
-    local inst=$(brew search | eval "fzf ${FZF_DEFAULT_OPTS} -m --header='[brew:install]'")
+bup() {
+  ### BREW + FZF
+  # uninstall multiple packages at once, async
+  # mnemonic [B]rew [C]lean [P]lugin (e.g. uninstall)
 
-    if [[ $inst ]]; then
-      for prog in $(echo $inst)
-      do brew install $prog
-      done
-    fi
-  }
+  local upd=$(brew leaves | eval "fzf ${FZF_DEFAULT_OPTS} -m --header='[brew:update]'")
 
-  bup() {
-    ### BREW + FZF
-    # uninstall multiple packages at once, async
-    # mnemonic [B]rew [C]lean [P]lugin (e.g. uninstall)
+  if [[ $upd ]]; then
+    for prog in $(echo $upd)
+    do brew upgrade $prog
+    done
+  fi
 
-    local upd=$(brew leaves | eval "fzf ${FZF_DEFAULT_OPTS} -m --header='[brew:update]'")
+}
 
-    if [[ $upd ]]; then
-      for prog in $(echo $upd)
-      do brew upgrade $prog
-      done
-    fi
-
-  }
-
-  # Renames files to lower case for current directory
-  rfl() {
-    for f in *; do mv "$f" "$f.tmp"; mv "$f.tmp" "`echo $f | tr "[:upper:]" "[:lower:]"`"; done
-  }
+# Renames files to lower case for current directory
+rfl() {
+  for f in *; do mv "$f" "$f.tmp"; mv "$f.tmp" "`echo $f | tr "[:upper:]" "[:lower:]"`"; done
+}
 
 
-	# FZF ripgrep
-  [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
-  export FZF_DEFAULT_OPS="--extended"
-	# Setting fd as the default source for fzf
-  export FZF_DEFAULT_COMMAND='fd --type f'
+# FZF ripgrep
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+export FZF_DEFAULT_OPS="--extended"
+# Setting fd as the default source for fzf
+export FZF_DEFAULT_COMMAND='fd --type f'
 
-	# To apply the command to CTRL-T as well
-  export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+# To apply the command to CTRL-T as well
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 
-	# Basic auto/tab completion
-	autoload -U compinit
-	zstyle ':completion:*' menu select
-	zmodload zsh/complist
-	compinit
-	_comp_options+=(globdots)
-	# Line above enables dotfiles to be found
+# Basic auto/tab completion
+autoload -U compinit
+zstyle ':completion:*' menu select
+zmodload zsh/complist
+compinit
+_comp_options+=(globdots)
+# Line above enables dotfiles to be found
 
-  # Fzf and gitbranches implementation
-  gcb() {
-    git checkout $(git branch -a | fzf| awk '{$1=$1};1')
-  }
+# Fzf and gitbranches implementation
+gcb() {
+  git checkout $(git branch -a | fzf| awk '{$1=$1};1')
+}
 
-  # Fzf and gitbranches implementation
-  gfb() {
-    git fetch $(git branch -a | fzf| awk '{$1=$1};1')
-  }
+# Fzf and gitbranches implementation
+gfb() {
+  git fetch $(git branch -a | fzf| awk '{$1=$1};1')
+}
 
+nvi() {
+  nvm install node --reinstall-packages-from=node
+}
 
-	ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=160'
-
-	source /Users/damianveltkamp/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-	source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
-
-#alias laptop='bash <(curl -s https://raw.githubusercontent.com/monfresh/laptop/master/laptop)'
-
-#export PATH="$HOME/.bin:$PATH"
-
-#eval "$(hub alias -s)"
-
-#source /usr/local/share/chruby/chruby.sh
-
-#source /usr/local/share/chruby/auto.sh -f /Users/damianveltkamp/.config/cani/completions/_cani.zsh ] && source /Users/damianveltkamp/.config/cani/completions/_cani.zsh
+ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=#888888'
