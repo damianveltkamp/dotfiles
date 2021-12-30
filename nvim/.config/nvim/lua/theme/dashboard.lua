c510a37d92af10b7465afce60bd1fb2b58115a19
Damian Veltkamp <damian.veltkamp@deptagency.com>
2021-12-30 14:16:43 +0100

    Fixed dashboard new pull request functionality

--- nvim/.config/nvim/lua/theme/dashboard.lua
+++ nvim/.config/nvim/lua/theme/dashboard.lua
@@ -1,3 +1,20 @@
+function openNewPullRequest()
+  local pathSubstrings = {}
+
+  local handle = io.popen("git rev-parse --show-toplevel")
+  local gitProjectRoot = handle:read("*a")
+  handle:close()
+
+  for token in string.gmatch(gitProjectRoot, "[^/]+") do
+    table.insert(pathSubstrings, token)
+  end
+
+  local tableLength = table.getn(pathSubstrings)
+  local projectName = string.gsub(pathSubstrings[tableLength], "%s+", "")
+  local executionString = 'open -a "Google chrome" ' .. 'https://bitbucket.org/tamtam-nl/' .. projectName .. '/pull-requests/new'
+
+  os.execute(executionString)
+end
 
 local opts = {
     custom_header = {
@@ -10,23 +27,29 @@ local opts = {
     },
     custom_section = {
       a = {
-        description = { '    BitBucket' },
+        description = { '    BitBucket   ' },
         command = 'silent !open -a "Google chrome" "https://bitbucket.com"',
       },
       b = {
-        description = { '    Gmail    ' },
-        command = 'silent !open -a "Google chrome" "https://gmail.com"',
+        description = { '    BitBucket PR' },
+        command = 'silent lua openNewPullRequest() ',
       },
       c = {
-        description = { '    Jira     ' },
-        command = 'silent !open -a "Google chrome" "https://dept-nl.atlassian.net/jira/your-work"',
+        description = { '    Gmail       ' },
+        command = 'silent !open -a "Google chrome" "https://gmail.com"',
       },
       d = {
-        description = { '    Spotify  ' },
+        description = { '    Jira        ' },
+        command = 'silent !open -a "Google chrome" "https://dept-nl.atlassian.net/jira/your-work"',
+      },
+      e = {
+        description = { '    Spotify     ' },
         command = 'silent !open -a spotify',
       },
     }
 }
+
+-- https://bitbucket.org/tamtam-nl/dtnl-total-energie-website/pull-requests/new
 
 vim.g.dashboard_custom_header = opts.custom_header
 vim.g.dashboard_custom_section = opts.custom_section
