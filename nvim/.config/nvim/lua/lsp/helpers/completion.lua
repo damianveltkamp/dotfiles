b51ad7fb979eeac5fb61a63d810308977aff51a0
Damian Veltkamp <damian.veltkamp@deptagency.com>
2023-03-14 12:36:30 +0100

    remove unused stuff

--- nvim/.config/nvim/lua/lsp/helpers/completion.lua
+++ nvim/.config/nvim/lua/lsp/helpers/completion.lua
@@ -22,10 +22,6 @@ local has_words_before = function()
 	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
-local feedkey = function(key, mode)
-	vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
-end
-
 -- Set completion menu options
 vim.opt.completeopt = "menuone,noselect"
 
@@ -51,8 +47,6 @@ cmp.setup({
 		["<Tab>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_next_item()
-			-- elseif vim.fn["vsnip#available"](1) == 1 then
-			-- 	feedkey("<Plug>(vsnip-expand-or-jump)", "")
 			elseif has_words_before() then
 				cmp.complete()
 			else
@@ -63,8 +57,6 @@ cmp.setup({
 		["<S-Tab>"] = cmp.mapping(function()
 			if cmp.visible() then
 				cmp.select_prev_item()
-				-- elseif vim.fn["vsnip#jumpable"](-1) == 1 then
-				-- 	feedkey("<Plug>(vsnip-jump-prev)", "")
 			end
 		end, { "i", "s" }),
 	},
