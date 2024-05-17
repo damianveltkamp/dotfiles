62d95adbb4076aceb6a773c3cbf7387bb45c119a
Damian Veltkamp <damian.veltkamp@deptagency.com>
2024-05-17 14:23:49 +0200

    bunch of stuff

--- nvim/.config/nvim/lua/plugins/completion.lua
+++ nvim/.config/nvim/lua/plugins/completion.lua
@@ -2,6 +2,7 @@ return {
   {
     'hrsh7th/nvim-cmp',
     event = 'InsertEnter',
+    dup = 0,
     config = function()
       local cmp_status_ok, cmp = pcall(require, 'cmp')
       if not cmp_status_ok then
@@ -23,7 +24,7 @@ return {
       }
 
       local has_words_before = function()
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        local line, col = table.unpack(vim.api.nvim_win_get_cursor(0))
         return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match '%s' == nil
       end
 
