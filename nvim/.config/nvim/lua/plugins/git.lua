c14b5b1ea6f1a1c5f3f748f7129db9f209f263ca
Damian Veltkamp <damian.veltkamp@deptagency.com>
2024-10-01 19:03:54 +0200

    bunch of stuff

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -1,10 +1,12 @@
 return {
-  'NeogitOrg/neogit',
-  dependencies = {
-    'nvim-lua/plenary.nvim', -- required
-    'sindrets/diffview.nvim', -- optional - Diff integration
-    'nvim-telescope/telescope.nvim', -- optional
+  {
+    'NeogitOrg/neogit',
+    dependencies = {
+      'nvim-lua/plenary.nvim', -- required
+      'sindrets/diffview.nvim', -- optional - Diff integration
+      'nvim-telescope/telescope.nvim', -- optional
+    },
+    config = true,
+    event = 'VeryLazy',
   },
-  config = true,
-  event = 'VeryLazy',
 }
