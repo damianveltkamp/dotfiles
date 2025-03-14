a96f5278485e8bf0d230355fb7ec8f7c9652c99e
Damian Veltkamp <damian.veltkamp@deptagency.com>
2025-03-14 12:06:25 +0100

    move snippets

--- nvim/.config/nvim/lua/plugins/snippets.lua
+++ nvim/.config/nvim/lua/plugins/snippets.lua
@@ -8,9 +8,11 @@ return {
   config = function()
     local luasnip_status_ok, luasnip = pcall(require, 'luasnip')
     if not luasnip_status_ok then
-      vim.notify('Not able to load in luasnip', 'error')
+      vim.notify('Not able to load in luasnip', vim.log.levels.Error)
       return
     end
+
+    require('luasnip.loaders.from_snipmate').lazy_load()
 
     luasnip.config.set_config {
       history = true,
@@ -38,42 +40,5 @@ return {
         luasnip.parser.parse_snippet('Expand', '--testing'),
       },
     }
-
-    luasnip.add_snippets(nil, {
-      typescriptreact = {
-        -- Generic javascript snippets
-        luasnip.parser.parse_snippet('switch', 'switch ($1) { \n  case "$2": { \n    $3 \n  } \n  $4 \n}'),
-        luasnip.parser.parse_snippet('lookuptable', 'const $1 = ($2) => ({\n$3\n});'),
-        luasnip.parser.parse_snippet('afunc', '($1) => {$2}'),
-
-        -- React specific snippets
-        luasnip.parser.parse_snippet('component', 'export type $1 = HTMLAttributes<HTMLElement> { \n $2 \n}\n\nexport const $3 = ({$4}: $5) => { \n};'),
-        luasnip.parser.parse_snippet('styles', 'import * as styles from "./styles.css"'),
-
-        -- Tanstack
-        luasnip.parser.parse_snippet('tanstackQuery', 'const { data, error } = useQuery({\n  queryKey: [$1],\n  queryFn: () => $2,\n});\n$3'),
-
-        -- Component doc
-        luasnip.parser.parse_snippet('doc', '/**\n * $1\n */'),
-      },
-      typescript = {
-        -- Generic javascript snippets
-        luasnip.parser.parse_snippet('switch', 'switch ($1) { \n  case "$2": { \n    $3 \n  } \n  $4 \n}'),
-        luasnip.parser.parse_snippet('lookuptable', 'const $1 = ($2) => ({\n$3\n});'),
-        luasnip.parser.parse_snippet('afunc', '($1) => {$2}'),
-
-        -- Tanstack
-        luasnip.parser.parse_snippet('tanstackQuery', 'const { data, error } = useQuery({\n  queryKey: [$1],\n  queryFn: () => $2,\n});\n$3'),
-
-        -- Vanilla extract snippets
-        luasnip.parser.parse_snippet('ve:mq', "'@media': {\n[$1]: {\n $2 \n}\n},"),
-        luasnip.parser.parse_snippet('ve:selectors', "selectors: {\n'$1': {\n $2 \n}\n},"),
-        luasnip.parser.parse_snippet('ve:boolean', '$1: {\n  true: {\n    $2 \n  },\n  false: {\n    $3 \n  }\n},'),
-        luasnip.parser.parse_snippet('ve:font', '...theme.fonts.$1'),
-
-        -- Component doc
-        luasnip.parser.parse_snippet('doc', '/**\n * $1\n  */'),
-      },
-    })
   end,
 }
