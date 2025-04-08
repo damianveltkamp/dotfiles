7fdcb4c85256d57076dc440b088b1d27fbc14f21
Damian Veltkamp <damian.veltkamp@deptagency.com>
2025-04-08 12:58:18 +0200

    bunch of stuff

--- nvim/.config/nvim/lua/plugins/linting.lua
+++ nvim/.config/nvim/lua/plugins/linting.lua
@@ -23,18 +23,3 @@ return {
     })
   end,
 }
-
--- return {
---   'mfussenegger/nvim-lint',
---   event = 'BufReadPost',
---   config = function()
---     local lint_status_ok, lint = pcall(require, 'lint')
---     if not lint_status_ok then
---       return
---     end
---
---     lint.linters_by_ft = {
---       markdown = { 'markdownlint' },
---     }
---   end,
--- }
