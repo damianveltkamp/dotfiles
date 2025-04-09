f2160f88bb6f16b69c63162a723d1f9785b73c13
Damian Veltkamp <damian.veltkamp@deptagency.com>
2025-04-09 12:30:55 +0200

    bunch of stuff

---
+++ nvim/.config/nvim/lua/plugins/ai.lua
@@ -0,0 +1,18 @@
+return {
+  -- TODO: start implementing codecompanion
+  -- {
+  --   'olimorris/codecompanion.nvim',
+  --   event = { 'BufReadPre', 'BufNewFile' },
+  --   dependencies = {
+  --     'nvim-lua/plenary.nvim',
+  --     'nvim-treesitter/nvim-treesitter',
+  --   },
+  --   opts = {
+  --     adapters = {
+  --       copilot = function()
+  --         return require('codecompanion.adapters').extend('copilot', {})
+  --       end,
+  --     },
+  --   },
+  -- },
+}
